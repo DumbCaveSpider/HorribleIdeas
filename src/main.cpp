9ad18433ed65451a5968ecef4769aeb921a53411
Cheeseworks <bluewitherer2422@gmail.com>
2025-07-24 11:30:21 -0500

    pogger

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,60 @@
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer)
-{
-public:
-    bool init() override
-    {
-        if (!MenuLayer::init())
-            return false;
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
 
-        auto bottomMenu = this->getChildByID("bottom-menu");
-        if (bottomMenu != nullptr)
-        {
+        if (auto bottomMenu = this->getChildByID("bottom-menu")) {
             auto btnSprite = CircleButtonSprite::createWithSpriteFrameName(
                 "GJ_moonsIcon_001.png",
                 1.0f,
                 CircleBaseColor::Green,
-                CircleBaseSize::MediumAlt);
+                CircleBaseSize::MediumAlt
+            );
 
             auto btn = CCMenuItemSpriteExtra::create(
                 btnSprite,
                 this,
-                menu_selector(MyMenuLayer::onHorribleButton));
+                menu_selector(MyMenuLayer::onHorribleButton)
+            );
             btn->setID("horribleBtn");
-            auto menu = typeinfo_cast<CCMenu *>(bottomMenu);
-            if (menu != nullptr)
-            {
+
+            if (auto menu = typeinfo_cast<CCMenu*>(bottomMenu)) {
                 menu->addChild(btn);
                 menu->updateLayout(true);
-            }
-        }
-        return true;
-    }
+            };
+        };
 
-    void onHorribleButton(CCObject *)
-    {
-        auto popup = HorriblePopupSelect::create();
-        popup->show();
-    }
+        /*
+        weird pog rub emoji
+        */
+
+        auto pogRubEmoji = LazySprite::create({ 20.f, 20.f }, false);
+        pogRubEmoji->setID("pogrubEmoji"_spr);
+
+        pogRubEmoji->setLoadCallback([this, pogRubEmoji](Result<> res) {
+            if (res.isOk()) {
+                // Success: scale and position the sprite
+                log::info("Sprite loaded successfully");
+            } else {
+                // Failure: set fallback image
+                log::error("Sprite failed to load, setting fallback: {}", res.unwrapErr());
+                pogRubEmoji->initWithSpriteFrameName("btn_chatHistory_001.png");
+            };
+
+            pogRubEmoji->setPosition({ getScaledContentSize().width - 12.5f, 12.5f });
+            pogRubEmoji->setAnchorPoint({ 1, 0 });
+            pogRubEmoji->setScale(0.875f);
+                                     });
+
+        pogRubEmoji->loadFromUrl("https://cdn.discordapp.com/emojis/831564390439321651.webp?size=1024&animated=true", LazySprite::Format::kFmtWebp, false);
+        SceneManager::get()->keepAcrossScenes(pogRubEmoji);
+
+        return true;
+    };
+
+    void onHorribleButton(CCObject*) {
+        if (auto popup = HorriblePopupSelect::create()) popup->show();
+    };
 };
--- src/popup/HorriblePopupSelect.cpp
+++ src/popup/HorriblePopupSelect.cpp
@@ -1,22 +1,22 @@
 #include <Geode/Geode.hpp>
 #include "HorriblePopupSelect.hpp"
 
-bool HorriblePopupSelect::setup()
-{
+bool HorriblePopupSelect::setup() {
     setTitle("Horrible Options");
+
+    log::debug("do stuff!");
 
     return true;
 };
 
-HorriblePopupSelect *HorriblePopupSelect::create()
-{
+HorriblePopupSelect* HorriblePopupSelect::create() {
     auto ret = new HorriblePopupSelect();
 
-    if (ret && ret->initAnchored(300.f, 280.f))
-    {
+    if (ret && ret->initAnchored(300.f, 280.f)) {
         ret->autorelease();
         return ret;
-    }
+    };
+
     delete ret;
     return nullptr;
 };
