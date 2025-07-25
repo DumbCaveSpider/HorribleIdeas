3bb6da3f15488a619ccabe3d0d3d4edeb389a4e6
DumbCaveSpider <markiel_giles@outlook.com>
2025-07-25 18:31:54 +0800

    ahh tired

--- src/popup/HorriblePopupSelect.cpp
+++ src/popup/HorriblePopupSelect.cpp
@@ -6,28 +6,63 @@
 
 using namespace geode::prelude;
 
-bool HorriblePopupSelect::setup() {
+bool HorriblePopupSelect::setup()
+{
     setID("options"_spr);
     setTitle("Horrible Options");
 
-    // just a test!
-    if (auto test = ModOption::create(
-        "test-option",
-        "Test Option",
-        "This is a test option.",
-        true
-    )) {
-        test->setPosition(12.5f, 200.f);
-        m_mainLayer->addChild(test);
-    };
+    auto mainLayerSize = m_mainLayer->getContentSize();
+
+    // Add a background sprite to the popup
+    auto OptionScollBg = CCScale9Sprite::create("square02_001.png");
+    OptionScollBg->setAnchorPoint({0.5f, 0.5f});
+    OptionScollBg->setPosition(mainLayerSize.width / 2, mainLayerSize.height / 2 - 10.f);
+    OptionScollBg->setContentSize({mainLayerSize.width - 25.f, mainLayerSize.height - 45.f});
+    OptionScollBg->setOpacity(50);
+    m_mainLayer->addChild(OptionScollBg);
+
+    // scroll layer
+    auto OptionsScrollLayer = ScrollLayer::create(CCSize(OptionScollBg->getContentSize().width, OptionScollBg->getContentSize().height));
+    OptionsScrollLayer->setID("scrollLayer");
+    OptionsScrollLayer->setScale(0.95f);
+    OptionsScrollLayer->setAnchorPoint({0.5f, 0.5f});
+    OptionsScrollLayer->setPosition({13.f, 13.f});
+    m_mainLayer->addChild(OptionsScrollLayer);
+
+    // Create a ColumnLayout for the mod options
+    auto optionsColumn = ColumnLayout::create()
+        ->setAxisAlignment(AxisAlignment::End)
+        ->setCrossAxisAlignment(AxisAlignment::Center)
+        ->setAutoGrowAxis(0.95f)
+        ->setGap(10.f);
+
+    auto optionsContent = OptionsScrollLayer->m_contentLayer;
+
+    std::vector<std::tuple<std::string, std::string, std::string, bool>> modOptions = {
+        {"mod1", "Mod One", "Description for Mod One.", false},
+        {"mod2", "Mod Two", "Description for Mod Two.", true},
+        {"mod3", "Mod Three", "Description for Mod Three.", false}};
+
+    for (const auto &option : modOptions)
+    {
+        const auto &[id, name, desc, enabled] = option;
+        auto modOption = ModOption::create(id, name, desc, enabled);
+        if (modOption != nullptr)
+        {
+            optionsContent->addChild(modOption);
+        }
+    }
+
 
     return true;
 };
 
-HorriblePopupSelect* HorriblePopupSelect::create() {
+HorriblePopupSelect *HorriblePopupSelect::create()
+{
     auto ret = new HorriblePopupSelect();
 
-    if (ret && ret->initAnchored(300.f, 280.f)) {
+    if (ret && ret->initAnchored(300.f, 280.f))
+    {
         ret->autorelease();
         return ret;
     };
--- src/popup/toggle/ModOption.cpp
+++ src/popup/toggle/ModOption.cpp
@@ -6,86 +6,100 @@
 
 using namespace geode::prelude;
 
-bool ModOption::init(std::string id, std::string name, std::string description, bool restart) {
+bool ModOption::init(std::string id, std::string name, std::string description, bool restart)
+{
     m_modID = id;
     m_modName = name;
     m_modDescription = description;
 
     m_restartRequired = restart;
 
-    if (!CCMenu::init()) return false;
+    if (!CCMenu::init())
+        return false;
 
     setID(m_modID);
-    setContentSize({ 125.f, 25.f });
-    setAnchorPoint({ 0, 1 });
+    // Use scroll layer width if available, otherwise fallback to 125.f
+    float optionWidth = 125.f;
+    if (auto parent = getParent()) {
+        // Try to get parent scroll layer's content size
+        auto scrollLayer = dynamic_cast<CCLayer*>(parent);
+        if (scrollLayer) {
+            optionWidth = scrollLayer->getContentSize().width;
+        }
+    }
+
+    setContentSize({optionWidth, 25.f});
+    setAnchorPoint({0, 1});
+
+    // Horizontal layout: [checkbox] [mod name] [info button]
+    float y = getScaledContentHeight() / 2.f;
 
     auto togglerOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
     togglerOff->setScale(0.875f);
-
     auto togglerOn = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
     togglerOn->setScale(0.875f);
-
     m_toggler = CCMenuItemToggler::create(
         togglerOff,
         togglerOn,
         this,
-        menu_selector(ModOption::onToggle)
-    );
+        menu_selector(ModOption::onToggle));
     m_toggler->setID("toggle");
-    m_toggler->setAnchorPoint({ 0.5f, 0.5f });
-    m_toggler->setPosition({ 7.5f, getScaledContentHeight() / 2.f });
+    m_toggler->setAnchorPoint({0.5f, 0.5f});
+    m_toggler->setPosition({0.f, y});
     m_toggler->setScale(0.625f);
-
     addChild(m_toggler);
 
     auto nameLabel = CCLabelBMFont::create(
         m_modName.c_str(),
         "bigFont.fnt",
-        getScaledContentWidth() - 25.f,
-        kCCTextAlignmentLeft
-    );
+        getScaledContentWidth() - 45.f,
+        kCCTextAlignmentLeft);
     nameLabel->setID("name");
     nameLabel->setLineBreakWithoutSpace(true);
-    nameLabel->setAnchorPoint({ 0.f, 0.5f });
-    nameLabel->setPosition({ 20.f, getScaledContentHeight() / 2.f });
+    nameLabel->setAnchorPoint({0.f, 0.5f});
+    nameLabel->setPosition({0.f + 20.f, y});
     nameLabel->setScale(0.4f);
-
     addChild(nameLabel);
 
+    // Place info button right after the mod name
+    float infoX = 20.f + nameLabel->getScaledContentSize().width + 15.f;
     auto descBtnSprite = CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png");
     descBtnSprite->setScale(0.375f);
-
     auto descBtn = CCMenuItemSpriteExtra::create(
         descBtnSprite,
         this,
-        menu_selector(ModOption::onDescription)
-    );
+        menu_selector(ModOption::onDescription));
     descBtn->setID("info");
-    descBtn->setAnchorPoint({ 0.5f, 0.5f });
-    descBtn->setPosition({ getScaledContentWidth() - 2.5f, getScaledContentHeight() - 2.5f });
-
+    descBtn->setAnchorPoint({0.5f, 0.5f});
+    descBtn->setPosition({infoX, y});
     addChild(descBtn);
 
     return true;
 };
 
-void ModOption::onToggle(CCObject*) {
-    if (m_toggler) getMod()->setSavedValue(m_modID, m_toggler->isToggled());
-    if (m_restartRequired) Notification::create("Restart required!", NotificationIcon::Warning, 2.5f)->show();
+void ModOption::onToggle(CCObject *)
+{
+    if (m_toggler)
+        getMod()->setSavedValue(m_modID, m_toggler->isToggled());
+    if (m_restartRequired)
+        Notification::create("Restart required!", NotificationIcon::Warning, 2.5f)->show();
 };
 
-void ModOption::onDescription(CCObject*) {
+void ModOption::onDescription(CCObject *)
+{
     if (auto popup = FLAlertLayer::create(
-        m_modName.c_str(),
-        m_modDescription.c_str(),
-        "OK"
-    )) popup->show();
+            m_modName.c_str(),
+            m_modDescription.c_str(),
+            "OK"))
+        popup->show();
 };
 
-ModOption* ModOption::create(std::string id, std::string name, std::string description, bool restart) {
+ModOption *ModOption::create(std::string id, std::string name, std::string description, bool restart)
+{
     auto ret = new ModOption();
 
-    if (ret && ret->init(id, name, description, restart)) {
+    if (ret && ret->init(id, name, description, restart))
+    {
         ret->autorelease();
         return ret;
     };
