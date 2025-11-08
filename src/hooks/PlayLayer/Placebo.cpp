d2bde1cc21c36fecc8645342b983eac055452b51
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2025-11-08 20:38:42 +0800

    placebo + gamber worked

--- src/hooks/PlayLayer/Placebo.cpp
+++ src/hooks/PlayLayer/Placebo.cpp
@@ -8,48 +8,59 @@
 using namespace geode::prelude;
 using namespace horrible;
 
-struct Fields {
+struct Fields
+{
     bool enabled = horribleideas::get("placebo");
 };
 
 Fields m_fields;
 
-void placeboEffect() {
-    if (horribleMod && m_fields.enabled) {
+void placeboEffect()
+{
+    log::info("Checking for placebo effect...");
+    if (horribleMod && m_fields.enabled)
+    {
         int rnd = randng::tiny() % 1000;
         log::info("placebo effect roll: {}", rnd);
 
-        if (rnd < 10) {
-            log::info("Placebo effect activated! Enabling all horrible mod options...");
-
-            for (const auto& option : options::getAll()) {
-                horribleideas::set(option.id, true);
-                log::debug("Enabled option: {}", option.id);
-            };
-        };
-
-        if (rnd < 5) {
+        if (rnd <= 10) // 1% chance :trol:
+        {
             log::info("Placebo effect activated! Disabling all horrible mod options...");
 
-            for (const auto& option : options::getAll()) {
-                // set all options to false
-                horribleideas::set(option.id, false);
-                log::debug("Disabled option: {}", option.id);
+            for (const auto &option : options::getAll())
+            {
+                // if the options is disabled, enabled it
+                if (horribleideas::get(option.id) == false)
+                {
+                    log::debug("Enabled option: {}", option.id);
+                    horribleideas::set(option.id, true);
+                }
+                else
+                {
+                    log::debug("Disabled option: {}", option.id);
+                    horribleideas::set(option.id, false);
+                }
             };
         };
     };
 };
 
-class $modify(PlaceboLevelPage, LevelPage) {
-    void onPlay(CCObject * sender) {
+class $modify(PlaceboLevelPage, LevelPage)
+{
+    void onPlay(CCObject *sender)
+    {
         placeboEffect();
+        log::debug("Placebo onPlay triggered");
         LevelPage::onPlay(sender);
     };
 };
 
-class $modify(PlaceboLevelInfoLayer, LevelInfoLayer) {
-    void onPlay(CCObject * sender) {
+class $modify(PlaceboLevelInfoLayer, LevelInfoLayer)
+{
+    void onPlay(CCObject *sender)
+    {
         placeboEffect();
+        log::debug("Placebo onPlay triggered");
         LevelInfoLayer::onPlay(sender);
     };
 };
--- src/hooks/PlayerObject/Gambler.cpp
+++ src/hooks/PlayerObject/Gambler.cpp
@@ -7,9 +7,13 @@
 using namespace geode::prelude;
 using namespace horrible;
 
-class $modify(GamblerPlayLayer, PlayLayer) {
-    struct Fields {
-        bool enabled = horribleMod->getSavedValue<bool>("gambler", false);
+class $modify(GamblerPlayLayer, PlayLayer)
+{
+    struct Fields
+    {
+        bool enabled = horribleideas::get("gambler");
+        bool triggered = false;
+        bool tricked = false;
     };
 
     bool init(GJGameLevel *level, bool useReplay, bool dontCreateObjects)
@@ -19,7 +23,63 @@ class $modify(GamblerPlayLayer, PlayLayer) {
         if (m_fields->enabled)
         {
             log::debug("gambler enabled");
+            // check every frame so we can detect each percentage change
+            if (!m_fields->triggered)
+            {
+                CCDirector::sharedDirector()->getScheduler()->scheduleSelector(
+                    schedule_selector(GamblerPlayLayer::gamblerCheck),
+                    this, 0.0f, false);
+            }
         }
         return true;
     };
+
+    // ensure that triggered is reset on level restart/full reset
+    void fullReset()
+    {
+        m_fields->triggered = false;
+        log::debug("gambler full reset");
+        PlayLayer::fullReset();
+    }
+
+    void resetLevel()
+    {
+        m_fields->triggered = false;
+        log::debug("gambler level reset");
+        PlayLayer::resetLevel();
+    }
+
+    void gamblerCheck(float)
+    {
+        if (!m_fields->enabled)
+            return;
+
+        int percentage = getCurrentPercentInt();
+        // detect the moment the player first reaches or crosses 95
+        if (percentage == 95 && !m_fields->triggered)
+        {
+            // roll a random number between 0 and 1
+            int roll = randng::fast() % 2;
+            log::info("Gambler roll: {}", roll);
+            if (roll == 0)
+            {
+                log::info("Gambler lost the bet!");
+                // reverse the player
+                m_player1->reversePlayer(nullptr);
+                m_player1->m_gravity = 0.01f; // reduce gravity to simulate a bounce
+                // force player to jump
+                GJBaseGameLayer::get()->handleButton(true, 1, true);
+                m_fields->triggered = true;
+                m_fields->tricked = true;
+                return;
+            }
+            else
+            {
+                log::info("Gambler won the bet! instant win.");
+                this->levelComplete();
+                m_fields->triggered = true;
+                return;
+            }
+        }
+    }
 };
